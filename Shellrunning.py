7fa79aa7f21870399fbbce22887d4d09c7560c43
Kyle Jarnagin <103604721+Jarnagoose@users.noreply.github.com>
2022-05-21 16:05:17 -0400

    Learned operators and notes are in flowcontrols

--- Shellrunning.py
+++ Shellrunning.py
@@ -1,11 +1,17 @@
 # This program says hello and asks for my name
 
-print('Hello World!')
+print('Hello World!')#Print makes strings appear on screen
 print('What is your name?') #ask for the name
-myName = input() #Variable is defined from user input
+myName = input() #Variable is defined from user input, input() for getting text from the keyboard.
 print('It is nice to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
 print('What is your age?') #ask for age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year')
+#str returns values as a string, which is required for string concatination
+#int returns values as integers, for math with other integers
+#float returns values as floating numbers, they have the decimal point, not a whole number
+#len takes a string value and reterns an integer value of the strings length
+#print displays the value passed to it.
+#functions are mini-programs inside the program.
---
+++ flowcontrolnotes.py
@@ -0,0 +1,39 @@
+#boolean datatype only has True and False, aka a binary oh boy
+#comparison operators
+#== equal to
+# != Not equal to
+# < less than
+# > greater than
+# <= less than or equal to
+# >= greater than or equal to
+#42 == 42
+#42 == 41
+# expressions with comparison operators evaluate to a boolean value
+#string values will not equal integer values
+#42 == '42'
+#float and integer however can be equal to eachother
+#20.00 == 20
+#Variable assignment operator is one equal sign
+#Equals to is two equal signs
+#Boolean operators are 'and' 'or' 'not'
+#True and True 
+#Truth tables for and
+# True and True evaluates to True
+# True and False == False
+# False and True == False
+# False and False == False
+#or operator becomes true as long as one of the requirements are met, only False if both are False
+#True or False
+#Truth tables for or
+# True or True == True
+# True or False == True
+# False or True == True
+# False or False == False
+#not False
+#The not operator only operates on one boolean value
+#Truth table for not
+#not True == False
+#not False == True
+myAge = 26
+myPet = 'dog'
+myAge > 20 and myPet == 'dog'
